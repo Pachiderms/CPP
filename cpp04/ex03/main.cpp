8db4f179ef173393be3bf75b74842f95f6eb7479
Telvin Zizi <tzizi@f1r3s9.clusters.42paris.fr>
2025-06-19 14:19:22 +0200

    finished

--- cpp04/ex03/main.cpp
+++ cpp04/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tzizi <tzizi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 15:24:24 by tzizi             #+#    #+#             */
-/*   Updated: 2025/06/19 11:11:20 by tzizi            ###   ########.fr       */
+/*   Updated: 2025/06/19 14:18:00 by tzizi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,83 +16,83 @@
 # include "MateriaSource.hpp"
 # include <iostream>
 
-int main ()
-{
-    Ice* test = new Ice();
-
-    AMateria* copieddIce = new Ice(*test);
-
-    ICharacter* hyoga = new Character("Seiya");
-    Character* dummy = new Character("Dummy");
-
-    std::cout << "Test Material Type: " << test->getType() << std::endl;
-    delete test;
-    
-    std::cout << "Cloned Material Type: " << copieddIce->getType() << std::endl;
-
-    hyoga->equip(copieddIce);
-
-    hyoga->use(0, *dummy);
-    hyoga->use(1, *dummy);
-
-    hyoga->unequip(0);
-    hyoga->use(0, *dummy);
-    
-    hyoga->equip(copieddIce);
-
-    hyoga->use(0, *dummy);
-
-    AMateria* clonedIce = copieddIce->clone();
-
-    hyoga->equip(clonedIce);
-
-    ICharacter* dumDummy = new Character(*dummy);
-    Cure* cure = new Cure();
-    AMateria* copyCure = new Cure(*cure);
-    AMateria* clonedIceyIce = clonedIce->clone();
-
-    hyoga->equip(cure);
-    hyoga->equip(copyCure);
-    hyoga->equip(clonedIceyIce);
-    
-    hyoga->use(1, *dummy);
-    hyoga->use(2, *dummy);
-    hyoga->use(3, *dummy);
-    hyoga->use(0, *dumDummy);
-    hyoga->use(4, *dummy);
-
-    hyoga->unequip(2);
-    hyoga->equip(clonedIceyIce);
-
-    hyoga->use(2, *dumDummy);
-
-    delete hyoga;
-    delete dummy;
-    delete dumDummy;
-    
-    delete cure;
-
-    return 0;
-}
-
-// int main()
+// int main ()
 // {
-//     IMateriaSource* src = new MateriaSource();
-//     src->learnMateria(new Ice());
-//     src->learnMateria(new Cure());
-//     ICharacter* me = new Character("me");
-//     AMateria* tmp;
-//     tmp = src->createMateria("ice");
-//     me->equip(tmp);
-//     tmp = src->createMateria("cure");
-//     me->equip(tmp);
-//     ICharacter* bob = new Character("bob");
-//     me->use(0, *bob);
-//     me->use(1, *bob);
+//     Ice* test = new Ice();
 
-//     delete bob;
-//     delete me;
-//     delete src;
+//     AMateria* copieddIce = new Ice(*test);
+
+//     ICharacter* hyoga = new Character("Seiya");
+//     Character* dummy = new Character("Dummy");
+
+//     std::cout << "Test Material Type: " << test->getType() << std::endl;
+//     delete test;
+    
+//     std::cout << "Cloned Material Type: " << copieddIce->getType() << std::endl;
+
+//     hyoga->equip(copieddIce);
+
+//     hyoga->use(0, *dummy);
+//     hyoga->use(1, *dummy);
+
+//     hyoga->unequip(0);
+//     hyoga->use(0, *dummy);
+    
+//     hyoga->equip(copieddIce);
+
+//     hyoga->use(0, *dummy);
+
+//     AMateria* clonedIce = copieddIce->clone();
+
+//     hyoga->equip(clonedIce);
+
+//     ICharacter* dumDummy = new Character(*dummy);
+//     Cure* cure = new Cure();
+//     AMateria* copyCure = new Cure(*cure);
+//     AMateria* clonedIceyIce = clonedIce->clone();
+
+//     hyoga->equip(cure);
+//     hyoga->equip(copyCure);
+//     hyoga->equip(clonedIceyIce);
+    
+//     hyoga->use(1, *dummy);
+//     hyoga->use(2, *dummy);
+//     hyoga->use(3, *dummy);
+//     hyoga->use(0, *dumDummy);
+//     hyoga->use(4, *dummy);
+
+//     hyoga->unequip(2);
+//     hyoga->equip(clonedIceyIce);
+
+//     hyoga->use(2, *dumDummy);
+
+//     delete hyoga;
+//     delete dummy;
+//     delete dumDummy;
+    
+//     delete cure;
 
 //     return 0;
 // }
+
+int main()
+{
+    IMateriaSource* src = new MateriaSource();
+    src->learnMateria(new Ice());
+    src->learnMateria(new Cure());
+    ICharacter* me = new Character("me");
+    AMateria* tmp;
+    tmp = src->createMateria("ice");
+    me->equip(tmp);
+    tmp = src->createMateria("cure");
+    me->equip(tmp);
+    ICharacter* bob = new Character("bob");
+    me->use(0, *bob);
+    me->use(1, *bob);
+
+    delete bob;
+    delete me;
+    delete src;
+
+    return 0;
+}
