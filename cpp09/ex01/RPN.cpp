aa585ebe10e26092a7db0e50f30d5b5c46d8db96
pachiderms <zizi.telvin@gmail.com>
2025-10-27 17:43:14 +0100

    parsing solved but to be checked

--- cpp09/ex01/RPN.cpp
+++ cpp09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 16:50:17 by tzizi             #+#    #+#             */
-/*   Updated: 2025/10/27 16:43:11 by marvin           ###   ########.fr       */
+/*   Updated: 2025/10/27 17:41:48 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,14 +31,26 @@ void RPN::calculate(const std::string& op)
     std::string signs = "+-/*";
     try{
         for (size_t i=0; i < op.length(); i++){
-            if ((signs.find(op[i], 0) == (size_t)-1 && isdigit(op[i]) <= 0) 
-                || (isdigit(op[i]) && (op[i] - '0') >= 10) || op[i] != ' '){
-                if (!_op.empty())
-                    _op.clear();
-                throw BadInputException();
+            if (op[i] != ' '){
+                std::string from = op.substr(i);
+                size_t space_index = from.find(" ");
+                std::string tmp = op.substr(i, space_index);
+                if (tmp.length() == 2 && tmp[0] == '-' && isdigit(tmp[1])){
+                    _op.push_back(((tmp[1] - '0') * -1) + '0');
+                    i += space_index;
+                }
+                else if ((tmp.length() > 1 || (signs.find(tmp[0], 0) == (size_t)-1 && isdigit(tmp[0]) <= 0))){
+                    if (!_op.empty())
+                        _op.clear();
+                    throw BadInputException();
+                }
+                else
+                    _op.push_back(tmp[0]);
             }
-        _op.push_back(op[i]);
         }
+        // for (size_t i=0; i < _op.size();i++)
+        //     std::cout << " " << _op[i] << "-> int: " << _op[i] - '0';
+        // std::cout << "\n";
         std::deque<char> res;
         while (!_op.empty())
         {
@@ -51,15 +63,18 @@ void RPN::calculate(const std::string& op)
             }
             res.push_back(_op.front());
             _op.pop_front();
+            // for (size_t i=0; i < res.size();i++)
+            //     std::cout << " " << res[i] << "-> int: " << res[i] - '0';
+            // std::cout << "res\n";
             sign = res.back();
             res.pop_back();
             b = res.back() - '0';
             res.pop_back();
             a = res.back() - '0';
             res.pop_back();
-            std::cout << "a: "<< a  << " b: " << b << " sign: "
-                 << sign << std::endl;
-            if (isdigit((char)a) <= 0 || isdigit((char)b) <= 0)
+            // std::cout << "a: "<< a  << " b: " << b << " sign: "
+            //      << sign << std::endl;
+            if (isdigit((char)a) < 0 || isdigit((char)b) < 0)
                 throw BadInputException();
             switch (sign)
             {
--- cpp09/ex01/main.cpp
+++ cpp09/ex01/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tzizi <tzizi@student.42.fr>                +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 16:44:57 by tzizi             #+#    #+#             */
-/*   Updated: 2025/10/22 18:11:11 by tzizi            ###   ########.fr       */
+/*   Updated: 2025/10/27 16:53:03 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,41 +18,6 @@ int main(int ac, char**av)
         return -1;
 
     RPN rpn;
-
-    // std::deque<char> _op;
-    // _op.push_back('3');
-    // _op.push_back('4');
-    // _op.push_back('-');
-    // _op.push_back('5');
-    // _op.push_back('+');
-
-    // for (size_t i=0;i<_op.size();i++){
-    //             std::cout << " " << _op[i] << std::endl;
-    // }
-
-    // int a = _op.front() - 48;
-    // std::cout << a << std::endl;
-    // _op.pop_front();
-    // std::cout << "pop_front()\n";
-
-    // for (size_t i=0;i<_op.size();i++){
-    //             std::cout << " " << _op[i] << std::endl;
-    // }
-
-
-    // int b = _op.front() - 48;
-    // std::cout << b << std::endl;
-    // _op.pop_front();
-
-    // _op.pop_front();
-    
-    // _op.push_front((a - b) + 48);
-    // std::cout << "push_front('/')\n";
-
-    // for (size_t i=0;i<_op.size();i++){
-    //             std::cout << " " << _op[i] << std::endl;
-    // }
-    // (void)av;
     rpn.calculate(av[1]);
     
     return 0;
