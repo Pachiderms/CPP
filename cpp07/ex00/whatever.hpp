0f278f80dd969993e255faafda85b267438944bb
Telvin Zizi <tzizi@f2r5s3.clusters.42paris.fr>
2026-01-15 17:07:31 +0100

    cpp07 according to correction

--- cpp07/ex00/whatever.hpp
+++ cpp07/ex00/whatever.hpp
@@ -11,11 +11,11 @@ void swap(T & x, T & y){
 }
 
 template <typename T>
-const T max(T x, T y){
-    return x > y ? x : y;
+const T* max(T &x, T &y){
+    return x > y ? &x : &y;
 }
 
 template <typename T>
-const T min(T x, T y){
-    return x < y ? x : y;
+const T* min(T &x, T &y){
+    return x < y ? &x : &y;
 }
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -6,33 +6,29 @@
 template <typename T>
 void myFun(T &e)
 {
-    std::cout << "My fun: " << e << std::endl;
+    std::cout << e << "/";
 }
+
+void Toupper(char &c){if (std::isalpha(c) != 0){c = std::toupper(c);}}
 
 int main ()
 {
-    char charArray[] = {'a', 'b', 'c', 'd'};
-    int intArray[] = { 1, 2, 3, 4};
+    char charArray[4] = {'a', 'b', 'c', 'd'};
+    int intArray[] = { 1, 2, 3, 4, 5, 6};
     std::string strArray[] = {"str1", "str2", "str3"};
 
 
-    for (int i=0; i < 4; i++){
-        std::cout << charArray[i] << "|";
-    }
+    ::iter(charArray, (int)sizeof(charArray), myFun);
     std::cout << "\n";
-    ::iter(charArray, 4, myFun);
+    ::iter(charArray, (int)sizeof(charArray), Toupper);
+    ::iter(charArray, (int)sizeof(charArray), myFun);
+    std::cout << "\n";
 
-    for (int i=0; i < 4; i++){
-        std::cout << intArray[i] << "|";
-    }
+    ::iter(intArray, (int)(sizeof(intArray)/sizeof(int)), myFun);
     std::cout << "\n";
-    ::iter(intArray, 4, myFun);
 
-    for (int i=0; i < 3; i++){ 
-        std::cout << strArray[i] << "|";
-    }
+    ::iter(strArray, (int)(sizeof(strArray)/sizeof(std::string)), myFun);
     std::cout << "\n";
-    ::iter(strArray, 3, myFun);
 
     return 0;
 }
